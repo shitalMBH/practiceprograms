555da42750280fa3a292a7acd388118d999fa6b3
Shital.Rasal <Shital.Rasal@ENCDAPPUNLT0539>
2024-12-10 13:11:06 +0530

    new array sort is introduced

---
+++ src/practicePrograms/Array/ReverseArraya.java
@@ -0,0 +1,20 @@
+package practicePrograms.Array;
+
+import java.util.Arrays;
+
+public class ReverseArraya {
+
+	public static void main(String[] args) {
+		int a[] = {5,2,1,0};
+	 int b [] = Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+		/*
+		int b[] =a.length;
+		for (int i =0; i=<a.length;i++)
+		{
+			for (int j = a.length;i>=0;i--) {};
+				if i*/
+		}
+
+	}
+
